a958e164d32502d362009e2995087a27f9056009
Matias Miranda <matias.miranda@merantix-momentum.com>
2025-01-18 13:24:25 +0100

    adding infra

---
+++ infrastructure/infrastructure_stack.py
@@ -0,0 +1,67 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    # aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_lambda as _lambda,
+    aws_apigateway as apigateway,
+    RemovalPolicy
+)
+from constructs import Construct
+
+
+class InfrastructureStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Create an S3 bucket
+        bucket = s3.Bucket(
+            self, "UserData",
+            versioned=True,
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True
+        )
+        
+        # Create a Lambda layer for Python packages
+        lambda_layer = _lambda.LayerVersion(
+            self, "Packages",
+            code=_lambda.Code.from_asset("src/lambda_layer"),
+            compatible_runtimes=[_lambda.Runtime.PYTHON_3_9],
+            description="A layer for Python dependencies"
+        )
+        
+        bot_lambda_function = _lambda.Function(
+            self, "BotFunction",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="main.handler",
+            code=_lambda.Code.from_asset("src/bot"),
+            environment={
+                "BUCKET_NAME": bucket.bucket_name
+            },
+            layers=[lambda_layer]
+        )
+        
+        # Grant Lambda permissions to write to the S3 bucket
+        bucket.grant_write(bot_lambda_function)
+
+        # Create an API Gateway
+        api = apigateway.RestApi(
+            self, "TelegramApi",
+            rest_api_name="TelegramApi",
+            description="API for Telegram Bot"
+        )
+
+        # Create /bot resource
+        bot_resource = api.root.add_resource("bot")
+
+        # Add POST method to /bot, integrated with the Lambda function
+        bot_resource.add_method(
+            "POST",
+            apigateway.LambdaIntegration(bot_lambda_function),
+            method_responses=[
+                apigateway.MethodResponse(
+                    status_code="200"
+                )
+            ]
+        )
