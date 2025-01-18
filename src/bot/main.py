5a13807f84222e72e53906e2124c4871909bbcf9
mg515 <miha.garafolj@gmail.com>
2025-01-18 13:32:37 +0100

    first lambda

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,16 +1,15 @@
 import json
 import logging
+import typing as t
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def lambda_handler(event, context):
-    body = json.loads(event['body'])
-
-    print("*** Received event", body)
+def lambda_handler(event: t.Dict, context: t.Dict) -> t.Dict:
+    logger.info(f"Event: {json.dumps(event)}")
 
     return {
-        'statusCode': 200,
-        'body': json.dumps(body)
+        "statusCode": 200,
+        "body": json.dumps({"message": "Hello from Lambda!"}),
     }
