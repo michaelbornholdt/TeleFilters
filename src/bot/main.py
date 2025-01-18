a2499623bcacb80d762f0fa449b94d283815d37b
mg515 <miha.garafolj@gmail.com>
2025-01-18 13:42:04 +0100

    first example lambda

--- src/bot/main.py
+++ src/bot/main.py
@@ -9,7 +9,15 @@ logger.setLevel(logging.INFO)
 def lambda_handler(event: t.Dict, context: t.Dict) -> t.Dict:
     logger.info(f"Event: {json.dumps(event)}")
 
+    chat_id = event["message"]["chat"]["id"]
+    user_name = event["message"]["from"]["username"]
+    message_text = event["message"]["text"]
+
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "Hello from Lambda!"}),
+        "body": json.dumps(
+            {
+                "message": f"Received message from {user_name} in chat {chat_id}: {message_text}"
+            }
+        ),
     }
